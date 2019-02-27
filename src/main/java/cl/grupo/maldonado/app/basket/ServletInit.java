53af18fd5aea309a55f2f4400213e9cf3d75c51a
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2019-02-27 11:56:50 -0300

    app engine

---
+++ src/main/java/cl/grupo/maldonado/app/basket/ServletInit.java
@@ -0,0 +1,12 @@
+package cl.grupo.maldonado.app.basket;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInit extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+}

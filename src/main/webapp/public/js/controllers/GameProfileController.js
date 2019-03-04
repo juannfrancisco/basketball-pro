346f357d1aee0e6a5f5599efac1b89c41327c587
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2019-03-04 11:44:31 -0300

    ramdom changes

--- src/main/webapp/public/js/controllers/GameProfileController.js
+++ src/main/webapp/public/js/controllers/GameProfileController.js
@@ -51,14 +51,55 @@ function($scope, $http, $routeParams)
     *
     **/
     $scope.calculateStats = function(){
-        debugger;
         var pointsLocal = 0;
+        var pointsVisitor = 0;
         for (var i in $scope.stats){
             if( $scope.stats[i].typeTeam == 'LOCAL' &&  $scope.stats[i].type == "PTS"){
                 pointsLocal = pointsLocal + $scope.stats[i].value;
+            }else{
+                pointsVisitor = pointsVisitor + $scope.stats[i].value;
             }
         }
         $scope.match.scoreLocal = pointsLocal;
+        $scope.match.scoreVisitor = pointsVisitor;
+        $scope.groupByQuarter();
+    }
+
+
+    $scope.statsPlayerLocal = {};
+    $scope.statsPlayerVisitor = {};
+
+    $scope.groupByPlayer = function(){
+
+        for (var i in $scope.stats){
+            if( $scope.stats[i].typeTeam == 'LOCAL' && $scope.stats[i].type == "PTS" ){
+                $scope.statsPlayerLocal[ $scope.stats[i].player.oid ] = $scope.statsPlayerLocal[ $scope.stats[i].player.oid ] ? [];
+                $scope.statsPlayerLocal[ $scope.stats[i].player.oid ].push();
+            }else{
+
+            }
+        }
+
+    }
+
+
+    $scope.scoreQuarterLocal = {};
+    $scope.scoreQuarterVisitor = {};
+
+    /**
+    *
+    */
+    $scope.groupByQuarter = function(){
+        for (var i in $scope.stats){
+            if( $scope.stats[i].typeTeam == 'LOCAL' && $scope.stats[i].type == "PTS" ){
+                var val = $scope.scoreQuarterLocal[$scope.stats[i].quarter] ?  $scope.scoreQuarterLocal[$scope.stats[i].quarter]:0;
+                $scope.scoreQuarterLocal[$scope.stats[i].quarter] = val + $scope.stats[i].value;
+            }else{
+                var val = $scope.scoreQuarterVisitor[$scope.stats[i].quarter] ?  $scope.scoreQuarterVisitor[$scope.stats[i].quarter]:0;
+                $scope.scoreQuarterVisitor[$scope.stats[i].quarter] = val + $scope.stats[i].value;
+            }
+        }
+
     }
 
 	$scope.loadData();
